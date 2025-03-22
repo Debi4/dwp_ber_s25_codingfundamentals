9e10dc931422041f37e639da58f108f04a0af555
Debi4 <tomadeb@gmail.com>
2025-03-22 12:22:17 +0000

    Lesson 3 work

---
+++ homework/3main.py
@@ -0,0 +1,19 @@
+Score = 97
+if Score >90:
+    print("A")
+elif Score >80:
+    print("B")
+elif Score <=90:
+    print("B")    
+
+Number = 100
+while Number <= 99:
+    print(Number)
+for oddnumbers in Number:
+    print(1,3,5, 100)
+
+my_name = input("What is your name?")
+print(my_name)
+
+number_values = input("What is a two digit number?")
+print()
